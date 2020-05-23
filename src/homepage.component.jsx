0c7bcbcb722f1bd2df6a97937dfff7352617d561
asif10388 <asif.khan10388@gmail.com>
2020-05-23 19:16:15 +0600

    initialized repository and added homepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="hompage">
+        <div className="directory-menu">
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Earphones</h1>
+                    <span className="subtitle">BUY NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Smart Watches</h1>
+                    <span className="subtitle">BUY NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Routers</h1>
+                    <span className="subtitle">BUY NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Airpods</h1>
+                    <span className="subtitle">BUY NOW</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Earpods</h1>
+                    <span className="subtitle">BUY NOW</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+)
+
+export default HomePage;

d98d1f963eb5a418bd8ba61668edadf27052a261
asif10388 <asif.khan10388@gmail.com>
2020-07-01 01:26:34 +0600

    modified dropdown components and fixed bugs

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,8 +4,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
 
     if(existingCartItem){
-        return cartItems.map(cartItem => cartItem.id ===cartItemToAdd?{...cartItem,quantity:cartItem.quantity + 1 }: cartItem)
+        return cartItems.map(cartItem => cartItem.id === cartItemToAdd.id ? {...cartItem , quantity:cartItem.quantity + 1 } : cartItem)
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity:1}]
+    return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }

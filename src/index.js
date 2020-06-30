bf06b0edf4a65563b877d544d9e4519b3486254c
asif10388 <asif.khan10388@gmail.com>
2020-06-30 21:32:09 +0600

    Added dropdown

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store = {store}>
-        <BrowserRouter>
-    <App />
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </Provider>,
 
-    
   document.getElementById('root')
 );
